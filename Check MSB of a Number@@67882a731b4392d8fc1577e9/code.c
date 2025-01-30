ceb4a8f1f70ad83400c33b931b4928307534ed8c
Pushkar Ahuja <pushkar0664.becse24@chitkara.edu.in>
2025-01-30 12:22:06 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;
-   scanf("%d",&a);
-   str c=a;
-if( str a[0]=='1'){
+    str a;
+   scanf("%s",&a);
+if( a[0]=='1'){
     printf("Set\n");
 }
 else {
