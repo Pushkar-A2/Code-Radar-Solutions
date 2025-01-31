5f47faa2de669ab24d62f8077d7529f87a5f579f
Pushkar Ahuja <pushkar0664.becse24@chitkara.edu.in>
2025-01-31 11:01:17 +0530

    Solution to Check Divisibility

--- Check Divisibility@@6788dd63490637cfc44e1b91/code.c
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -7,7 +7,7 @@ if(num % 5 == 0 && num % 11 == 0){
     printf("%d is divisible by both 5 and 11\n",num);
 }
 else{
-    printf("%d is not not divisible by 5 and 11\n",num);
+    printf("%d is not divisible by 5 and 11\n",num);
 }   
     return 0;
 }
