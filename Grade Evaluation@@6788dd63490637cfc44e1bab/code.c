f96d32188daee2ab82fb7b69d5fba56166050275
Pushkar Ahuja <pushkar0664.becse24@chitkara.edu.in>
2025-02-03 11:32:41 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char grade;
+    scanf("%c",&grade);
+
+    switch(grade){
+        case 'A':
+        printf("Excellent");
+        break;
+
+        case 'B':
+        printf("Good");
+        break;
+
+        case 'c':
+        printf("Average");
+        break;
+
+        case 'D':
+        printf("Below Average");
+        break;
+
+        case 'F':
+        printf("Fail");
+        break;
+
+        default:
+        printf("Invalid grade");
+        break;
+    }
+    return 0;
+}
